98693414db473cfb26e74dea8e7edf91b95e010e
SUBEENKANG <ksb970813@gmail.com>
2022-10-20 23:35:11 -0400

    final commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,14 @@
+/* 
+FileName: app.css
+Name: Subeen Kang
+StudentNumber: 301185495 
+Date: Oct 22, 2022.
+*/
+
+//IIFE -- Immediately Invoked Function Expression
+(function () {
+    function Start() {
+        console.log("App Started...");
+    }
+    window.addEventListener("load", Start);
+})();
